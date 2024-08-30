34a5c7e37cc393d839bc1dd6b2a1abe47598f6c8
Ayu2611 <ayushichouhan2611@gmail.com>
2024-08-30 15:30:32 +0000

    rep1

---
+++ 0basics/test1.js
@@ -0,0 +1,11 @@
+// alert(3+3) // we using nodejs ,not browser
+ /*number  2 to power 53 
+ boolean   true/false
+ bigInt  big numbers
+ null  
+ string  
+ undefined 
+ symbol unique react for components
+ object */ 
+ // console.log(typeof undefined) undefined
+ // console.log(typeof null) object 
---
+++ 0basics/test2.js
@@ -0,0 +1,35 @@
+ //datatype conversion
+//   let score = "3abc"
+//   console.log(typeof score)   //string
+   
+//   let valueInNumber = Number(score)
+//   console.log(typeof valueInNumber) //  number
+ 
+
+//   console.log(valueInNumber) //NaN not a number
+  //let score = "3abc" it should not convert in the number
+
+  /*let score  = null ,  0 since null is an object then when it convert it into number then it becomes zero
+  let score  = undefined ,NaN
+  let score = true , 1
+  let score = false , 0
+  let score = "ayushi", NaN
+  "33" => 33
+  "33abc"=> NaN 
+  */
+ /*let isLoggedIn = 1
+   let isLoggedIn = "" , false
+ let isLoggedIn = "ayushi" ,true
+ let booleaIsLoggedIn = Boolean(isLoggedIn)
+ console.log( booleaIsLoggedIn ) true*/
+
+/* let number = 33
+  let stringnumber = String(number)  33 
+  console.log(stringnumber);   
+  console.log(typeof stringnumber); string*/
+   
+  
+
+
+
+   
